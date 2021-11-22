2d7aca24f4a3fbae48e7cb9100d772236560fbf4
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-11-22 16:15:57 +0100

    Fix eslintrc errors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,85 +6,286 @@ import Profiles from './Profiles';
 import Rockets from './Rockets';
 
 const Navbar = () => {
-    const links = [{
-            id: 1,
-            path: '/',
-            text: 'Rockets',
-            name: 'rockets',
-        },
-        {
-            id: 2,
-            path: '/missions',
-            text: 'Missions',
-            name: 'missions',
-        },
-        {
-            id: 3,
-            path: '/my-profile',
-            text: 'My Profile',
-            name: 'my-profile',
-        },
-    ];
+  const links = [{
+    id: 1,
+    path: '/',
+    text: 'Rockets',
+    name: 'rockets',
+  },
+  {
+    id: 2,
+    path: '/missions',
+    text: 'Missions',
+    name: 'missions',
+  },
+  {
+    id: 3,
+    path: '/my-profile',
+    text: 'My Profile',
+    name: 'my-profile',
+  },
+  ];
 
-    return ( <
-        nav className = "navbar" >
-        <
-        header className = "logo-container" >
-        <
-        img src = { planet }
-        alt = "Logo"
-        className = "logo" /
-        >
-        <
-        h1 className = "title" > { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
-        Space Travelers & rsquo; Hub { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
+  return (
+    <nav className="navbar">
+      <header className="logo-container">
+        <img
+          src={planet}
+          alt="Logo"
+          className="logo"
+        />
+        <h1 className="title">
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          Space Travelers & rsquo; Hub
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
+          {' '}
+          { ' ' }
 
-        <
-        /h1> <
-        /header> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
-        ul className = "links" > { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } {
+        </h1>
+      </header>
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      <ul className="links">
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        {
             links.map((link) => (
-                // eslint-disable-next-line react/self-closing-comp
-                <
-                li key = { link.id }
-                className = { link.name } >
-                <
-                /li>
+              // eslint-disable-next-line react/self-closing-comp
+              <li
+                key={link.id}
+                className={link.name}
+              >
+              </li>
             ))
-        } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
+        }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
 
-        <
-        /ul> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
-        div >
-        <
-        NavLink to = { links.path }
-        key = { links.id }
-        style = {
+      </ul>
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      <div>
+        <NavLink
+          to={links.path}
+          key={links.id}
+          style={
             (isActive) => ({
-                color: isActive ? 'blue' : '',
+              color: isActive ? 'blue' : '',
             })
-        } >
-        { links.name } <
-        /NavLink> <
-        /div> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
-        Switch >
-        <
-        Route exact path = "/"
-        component = { Rockets }
-        /> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
-        Route path = "/missions"
-        component = { Missions }
-        /> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
-        Route path = "/profiles"
-        component = { Profiles }
-        /> { ' ' } { ' ' } { ' ' } { ' ' }
+        }
+        >
+          { links.name }
+        </NavLink>
+      </div>
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      { ' ' }
+      {' '}
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Rockets}
+        />
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        <Route
+          path="/missions"
+          component={Missions}
+        />
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        <Route
+          path="/profiles"
+          component={Profiles}
+        />
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
+        {' '}
+        { ' ' }
 
-        <
-        /Switch>
+      </Switch>
 
-        <
-        /nav>
-    );
+    </nav>
+  );
 };
 
 export default Navbar;
