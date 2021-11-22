5a8d5a9ae1abcce5c323acea193ed90b73e75cee
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-11-22 16:15:11 +0100

    Add paths to the navBar.js component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,182 +6,85 @@ import Profiles from './Profiles';
 import Rockets from './Rockets';
 
 const Navbar = () => {
-  const links = [{
-    id: 1,
-    path: '/',
-    text: 'Rockets',
-    name: 'rockets',
-  },
-  {
-    id: 2,
-    path: '/missions',
-    text: 'Missions',
-    name: 'missions',
-  },
-  {
-    id: 3,
-    path: '/my-profile',
-    text: 'My Profile',
-    name: 'my-profile',
-  },
-  ];
-
-  return (
-    <nav className="navbar">
-      <header className="logo-container">
-        <img
-          src={planet}
-          alt="Logo"
-          className="logo"
-        />
-        <h1 className="title">
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          Space Travelers & rsquo; Hub
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-          {' '}
-          { ' ' }
-
-        </h1>
-      </header>
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      <ul className="links">
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
+    const links = [{
+            id: 1,
+            path: '/',
+            text: 'Rockets',
+            name: 'rockets',
+        },
         {
-            links.map((link) => (
-              // eslint-disable-next-line react/self-closing-comp
-              <li
-                key={link.id}
-                className={link.name}
-              >
-              </li>
-            ))
-        }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
+            id: 2,
+            path: '/missions',
+            text: 'Missions',
+            name: 'missions',
+        },
+        {
+            id: 3,
+            path: '/my-profile',
+            text: 'My Profile',
+            name: 'my-profile',
+        },
+    ];
 
-      </ul>
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      <div>
-        <NavLink
-          to={links.path}
-          key={links.id}
-          style={
-            (isActive) => ({
-              color: isActive ? 'blue' : '',
-            })
-        }
+    return ( <
+        nav className = "navbar" >
+        <
+        header className = "logo-container" >
+        <
+        img src = { planet }
+        alt = "Logo"
+        className = "logo" /
         >
-          { links.name }
-        </NavLink>
-      </div>
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      { ' ' }
-      {' '}
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={Rockets}
-        />
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        <Route
-          path="/fuel-savings"
-          component={Missions}
-        />
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
-        {' '}
-        <Route
-          path="/about"
-          component={Profiles}
-        />
-        {' '}
-        { ' ' }
-        {' '}
-        { ' ' }
+        <
+        h1 className = "title" > { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
+        Space Travelers & rsquo; Hub { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
 
-      </Switch>
+        <
+        /h1> <
+        /header> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
+        ul className = "links" > { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } {
+            links.map((link) => (
+                // eslint-disable-next-line react/self-closing-comp
+                <
+                li key = { link.id }
+                className = { link.name } >
+                <
+                /li>
+            ))
+        } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' }
 
-    </nav>
-  );
+        <
+        /ul> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
+        div >
+        <
+        NavLink to = { links.path }
+        key = { links.id }
+        style = {
+            (isActive) => ({
+                color: isActive ? 'blue' : '',
+            })
+        } >
+        { links.name } <
+        /NavLink> <
+        /div> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
+        Switch >
+        <
+        Route exact path = "/"
+        component = { Rockets }
+        /> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
+        Route path = "/missions"
+        component = { Missions }
+        /> { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } { ' ' } <
+        Route path = "/profiles"
+        component = { Profiles }
+        /> { ' ' } { ' ' } { ' ' } { ' ' }
+
+        <
+        /Switch>
+
+        <
+        /nav>
+    );
 };
 
 export default Navbar;
