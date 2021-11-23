5282675608ec6274e9f7e1ce231ca2b7bc0d61dd
alexander16108 <alexandermayowa05@gmail.com>
2021-11-23 10:46:40 +0100

    adding actions into actions.js files

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,15 @@
-// import axios from 'axios';
-// import * as types from './types';
+import axios from 'axios';
+import { GET_ROCKETS, SET_ROCKETS, ERROR_ROCKETS }  from './types';
+
+export default function getRocketsAction() {
+  async (dispatch) => {
+  const baseUrl = 'https://api.spacexdata.com/v3/rockets';
+  try {
+    dispatch({ type: GET_ROCKETS });
+    const rockets = await axios.get(`${baseUrl}`);
+    dispatch({ type: SET_ROCKETS, payload: rockets });
+  } catch (error) {
+    dispatch({ type: ERROR_ROCKETS, payload: error.message });
+  }
+}
+};
--- src/store/types.js
+++ src/store/types.js
@@ -1,17 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import axios from 'axios';
+
 
 const GET_ROCKETS = 'space-travelers-hub/rockets/GET_ROCKETS';
 const SET_ROCKETS = 'space-travelers-hub/rockets/SET_ROCKETS';
 const ERROR_ROCKETS = 'space-travelers-hub/rockets/ERROR_ROCKETS';
-
-export const getRocketsAction = () => async (dispatch) => {
-  const baseUrl = 'https://api.spacexdata.com/v3/rockets';
-  try {
-    dispatch({ type: GET_ROCKETS });
-    const rockets = await axios.get(`${baseUrl}`);
-    dispatch({ type: SET_ROCKETS, payload: rockets });
-  } catch (error) {
-    dispatch({ type: ERROR_ROCKETS, payload: error.message });
-  }
-};
