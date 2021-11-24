6a7727b5f5d7b85685a92d5a5c83e705c6fe1c4a
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-11-24 17:35:30 +0100

    Add conditional rendering

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -11,44 +11,58 @@ import {
 const selectMissions = (state) => state.missionsReducer;
 
 const Missions = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getMissions());
-    }, []);
-    const selectedMissions = useSelector(selectMissions);
-    console.log(selectedMissions);
-    return ( <
-        div >
-        <
-        Table striped bordered hover size = "sm"
-        className = "table" >
-        <
-        thead >
-        <
-        tr >
-        <
-        th > Mission < /th> <
-        th > Description < /th> <
-        th > Status < /th> <
-        th > {} < /th> <
-        /tr> <
-        /thead> <
-        tbody > {
-            selectedMissions.map((mission) => ( <
-                tr key = { uuidv4() } > { console.log(m.isReserved) } <
-                td className = "mission-name" > { mission.mission_name } < /td> <
-                td > { mission.mission_description } < /td>
-
+        const dispatch = useDispatch();
+        useEffect(() => {
+            dispatch(getMissions());
+        }, []);
+        const selectedMissions = useSelector(selectMissions);
+        console.log(selectedMissions);
+        return ( <
+                div >
                 <
-                /tr>
-            ))
-        }
+                Table striped bordered hover size = "sm"
+                className = "table" >
+                <
+                thead >
+                <
+                tr >
+                <
+                th > Mission < /th> <
+                th > Description < /th> <
+                th > Status < /th> <
+                th > {} < /th> <
+                /tr> <
+                /thead> <
+                tbody > {
+                    selectedMissions.map((mission) => ( <
+                            tr key = { uuidv4() } > { console.log(m.isReserved) } <
+                            td className = "mission-name" > { mission.mission_name } < /td> <
+                            td > { mission.mission_description } < /td> <
+                            td className = "status"
+                            width = "100px" > {
+                                mission.isReserved ? < Badge bg = "success" > ACTIVE MEMBER < /Badge> :
+                                    < Badge bg = "secondary" > NOT A MEMBER < /Badge>} <
+                                    /td> <
+                                    td className = "status"
+                                width = "150px" > {
+                                    mission.isReserved ? < Button variant = "outline-danger"
+                                    onClick = {
+                                        () => dispatch(leaveMission(mission.mission_id)) } > Leave Mission < /Button> :
+                                        < Button variant = "outline-dark"
+                                    onClick = {
+                                        () => dispatch(joinMission(mission.mission_id)) } > Join Mission < /Button>} <
+                                    /td>
 
-        <
-        /tbody> <
-        /Table> <
-        /div>
-    );
-};
+                                    <
+                                    /tr>
+                                ))
+                        }
 
-export default Missions;
+                        <
+                        /tbody> <
+                        /Table> <
+                        /div>
+                    );
+                };
+
+                export default Missions;
