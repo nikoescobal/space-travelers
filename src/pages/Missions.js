9f0dda0e636b0f5df7b749234bb015c448cf1c56
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-11-24 17:18:51 +0100

    Add leave mission and dispatch mission

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -6,62 +6,49 @@ import { useSelector, useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import {
     getMissions,
-    joinMission,
 } from "../store/reducers/actions/missionActions";
 
 const selectMissions = (state) => state.missionsReducer;
 
 const Missions = () => {
-        const dispatch = useDispatch();
-        useEffect(() => {
-            dispatch(getMissionsFromApi());
-        }, []);
-        const selectedMissions = useSelector(selectMissions);
-        console.log(selectedMissions);
-        return ( <
-                div >
-                <
-                Table striped bordered hover size = "sm"
-                className = "table" >
-                <
-                thead >
-                <
-                tr >
-                <
-                th > Mission < /th> <
-                th > Description < /th> <
-                th > Status < /th> <
-                th > {} < /th> <
-                /tr> <
-                /thead> <
-                tbody > {
-                    selectedMissions.map((mission) => ( <
-                            tr key = { uuidv4() } > { console.log(m.isReserved) } <
-                            td className = "mission-name" > { mission.mission_name } < /td> <
-                            td > { mission.mission_description } < /td> <
-                            td className = "status"
-                            width = "100px" > {
-                                mission.isReserved ? < Badge bg = "success" > ACTIVE MEMBER < /Badge> :
-                                    < Badge bg = "secondary" > NOT A MEMBER < /Badge>} <
-                                    /td> <
-                                    td className = "status"
-                                width = "150px" > {
-                                    mission.isReserved ? < Button variant = "outline-danger"
-                                    onClick = {
-                                        () => dispatch(leaveMission(mission.mission_id)) } > Leave Mission < /Button> :
-                                        < Button variant = "outline-dark"
-                                    onClick = {
-                                        () => dispatch(joinMission(mission.mission_id)) } > Join Mission < /Button>} <
-                                    /td> <
-                                    /tr>
-                                ))
-                        }
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getMissions());
+    }, []);
+    const selectedMissions = useSelector(selectMissions);
+    console.log(selectedMissions);
+    return ( <
+        div >
+        <
+        Table striped bordered hover size = "sm"
+        className = "table" >
+        <
+        thead >
+        <
+        tr >
+        <
+        th > Mission < /th> <
+        th > Description < /th> <
+        th > Status < /th> <
+        th > {} < /th> <
+        /tr> <
+        /thead> <
+        tbody > {
+            selectedMissions.map((mission) => ( <
+                tr key = { uuidv4() } > { console.log(m.isReserved) } <
+                td className = "mission-name" > { mission.mission_name } < /td> <
+                td > { mission.mission_description } < /td>
 
-                        <
-                        /tbody> <
-                        /Table> <
-                        /div>
-                    );
-                };
+                <
+                /tr>
+            ))
+        }
 
-                export default Missions;
+        <
+        /tbody> <
+        /Table> <
+        /div>
+    );
+};
+
+export default Missions;
