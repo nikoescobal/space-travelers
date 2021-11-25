d1f421f558593c8adc24ed28123b49f9b16efdf4
Niko Escobal <niko.escobal@gmail.com>
2021-11-25 17:09:05 +0800

    Add logic for rendering reserved dragons on profile

---
+++ src/components/Profile.js
@@ -0,0 +1,3 @@
+const Profile = () => <h1>Profile coming soon... </h1>;
+
+export default Profile;
---
+++ src/pages/Profile.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { getRocketsAction } from '../store/reducers/actions/actions';
+import { fetchData } from '../store/reducers/actions/dragonActions';
+
+// const Profile = () => {
+//   const { rockets } = useSelector((state) => state.rockets);
+
+//   useEffect(() => {
+//     if (!rockets) {
+//       dispatch(getRocketsAction());
+//     }
+//   }, []);
+
+//   return (
+//     <section className="my-profile inner-padding">
+//       <div>
+//         <h1>My Missions</h1>
+//       </div>
+//       <div className="rockets">
+//         <h1>My Rockets</h1>
+//         <ul className="list-container">
+//           {rockets
+//             && rockets.filter((rocket) => (
+//               <li key={rocket.id} className="list-item">
+//                 {rocket.rocket_name}
+//               </li>
+//             ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// };
+// export default Profile;
+
+const Profile = () => {
+  const dispatch = useDispatch();
+  const dragons = useSelector((state) => state.dragons.dragons);
+
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved === true);
+
+  useEffect(() => {
+    if (!dragons.length) {
+      dispatch(fetchData);
+    }
+  }, []);
+
+  return (
+    <div>
+      {reservedDragons.length ? (
+        <>
+          <h2>My Reserved Dragons</h2>
+          <ul>
+            {reservedDragons.map((dragon) => (
+              <li key={dragon.name}>{dragon.name}</li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h2>There are currently no dragons reserved.</h2>
+      )}
+    </div>
+  );
+};
+export default Profile;
