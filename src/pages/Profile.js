f3edc4abce1d4b6f78978cd4f3a9f78f59a5c104
alexander16108 <alexandermayowa05@gmail.com>
2021-11-26 16:13:49 +0100

    Display filtered reserved rockets and style my-profile component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,39 +1,39 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRocketsAction } from '../store/reducers/actions/rocketActions';
+// import { getRocketsAction } from '../store/reducers/actions/rocketActions';
 import { fetchData } from '../store/reducers/actions/dragonActions';
 
-export const RocketProfile = () => {
-  const dispatch = useDispatch();
-  const { rockets } = useSelector((state) => state.rockets);
+// export const RocketProfile = () => {
+//   const dispatch = useDispatch();
+//   const { rockets } = useSelector((state) => state.rockets);
 
-  useEffect(() => {
-    if (!rockets) {
-      dispatch(getRocketsAction());
-    }
-  }, []);
+//   useEffect(() => {
+//     if (!rockets) {
+//       dispatch(getRocketsAction());
+//     }
+//   }, []);
 
-  return (
-    <section className="my-profile inner-padding">
-      <div>
-        <h1>My Missions</h1>
-      </div>
-      <div className="rockets">
-        <h1>My Rockets</h1>
-        <ul className="list-container">
-          {rockets
-            && rockets.filter((rocket) => (
-              <li key={rocket.id} className="list-item">
-                {rocket.rocket_name}
-              </li>
-            ))}
-        </ul>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="my-profile inner-padding">
+//       <div>
+//         <h1>My Missions</h1>
+//       </div>
+//       <div className="rockets">
+//         <h1>My Rockets</h1>
+//         <ul className="list-container">
+//           {rockets
+//             && rockets.filter((rocket) => (
+//               <li key={rocket.id} className="list-item">
+//                 {rocket.rocket_name}
+//               </li>
+//             ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// };
 
-export const DragonProfile = () => {
+const Profile = () => {
   const dispatch = useDispatch();
   const dragons = useSelector((state) => state.dragons.dragons);
 
@@ -62,3 +62,4 @@ export const DragonProfile = () => {
     </div>
   );
 };
+export default Profile;
