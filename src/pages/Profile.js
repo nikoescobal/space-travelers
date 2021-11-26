ed5f6e99d5b6c619ff9b7c0a67456f80c2d28a84
alexander16108 <alexandermayowa05@gmail.com>
2021-11-26 20:40:04 +0100

    adding rockets into my profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,43 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { getRocketsAction } from '../store/reducers/actions/rocketActions';
+import { getRocketsAction } from '../store/reducers/actions/rocketActions';
 import { fetchData } from '../store/reducers/actions/dragonActions';
-
-// export const RocketProfile = () => {
-//   const dispatch = useDispatch();
-//   const { rockets } = useSelector((state) => state.rockets);
-
-//   useEffect(() => {
-//     if (!rockets) {
-//       dispatch(getRocketsAction());
-//     }
-//   }, []);
-
-//   return (
-//     <section className="my-profile inner-padding">
-//       <div>
-//         <h1>My Missions</h1>
-//       </div>
-//       <div className="rockets">
-//         <h1>My Rockets</h1>
-//         <ul className="list-container">
-//           {rockets
-//             && rockets.filter((rocket) => (
-//               <li key={rocket.id} className="list-item">
-//                 {rocket.rocket_name}
-//               </li>
-//             ))}
-//         </ul>
-//       </div>
-//     </section>
-//   );
-// };
 
 const Profile = () => {
   const dispatch = useDispatch();
   const dragons = useSelector((state) => state.dragons.dragons);
+  const { rockets } = useSelector((state) => state.rockets);
 
-  const reservedDragons = dragons.filter((dragon) => dragon.reserved === true);
+  useEffect(() => {
+    if (!rockets) {
+      dispatch(getRocketsAction());
+    }
+  }, []);
 
   useEffect(() => {
     if (!dragons.length) {
@@ -47,30 +22,30 @@ const Profile = () => {
 
   return (
     <div>
-      {reservedDragons.length ? (
-        <>
-          <table className="table-fixed mx-auto w-full">
-            <thead>
-              <tr>
-                <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Dragons</th>
-                <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Rockets </th>
-                <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Missions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className=" flex flex-col px-6 py-3">
-                {reservedDragons.map((dragon) => (
-                  <td className="border-gray-400 border px-6 py-3" key={dragon.name}>{dragon.name}</td>
+      <table className="table-fixed mx-auto w-full">
+        <thead>
+          <tr>
+            <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Dragons</th>
+            <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Rockets </th>
+            <th className="w-1/3 space-x-3 text-2xl p-3 font-bold">My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className=" flex flex-col px-6 py-3">
+            {dragons.filter((dragon) => dragon.reserved === true)
+              .map((reserve) => (
+                <td className="border-gray-400 border px-6 py-3" key={reserve.name}>{reserve.name}</td>
+              ))}
+          </tr>
+          <tr className=" flex flex-col px-6 py-3">
+            {rockets
+              && rockets.filter((rocket) => rocket.reserved === true)
+                .map((filtered) => (
+                  <td className="border-gray-400 border px-6 py-3" key={filtered.name}>{filtered.name}</td>
                 ))}
-              </tr>
-            </tbody>
-          </table>
-        </>
-      ) : (
-        <h2 className="p-3 text-2xl font-bold">
-          There are currently no dragons reserved.
-        </h2>
-      )}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
