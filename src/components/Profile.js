fcbe4e3d401b408df74bfa8965e6772509f32574
Alexander <60612329+alexander16108@users.noreply.github.com>
2021-11-27 08:51:24 +0100

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getRocketsAction } from "../store/reducers/actions/rocketActions";
-import { fetchData } from "../store/reducers/actions/dragonActions";
-import { fetchAllMissions } from "../store/reducers/actions/missionActions";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRocketsAction } from '../store/reducers/actions/rocketActions';
+import { fetchData } from '../store/reducers/actions/dragonActions';
+import { fetchAllMissions } from '../store/reducers/actions/missionActions';
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -29,55 +29,49 @@ const Profile = () => {
     <div className="grid grid-cols-3 w-full mx-auto">
       <section>
         <h2 className="text-2xl w-full flex justify-center items-center p-3 font-bold">
-          My Rockets{" "}
-        </h2>{" "}
+          My Rockets
+        </h2>
         <ul className=" flex flex-col px-6 py-3">
-          {" "}
-          {rockets &&
-            rockets
+          {rockets
+            && rockets
               .filter((rocket) => rocket.reserved === true)
               .map((filtered) => (
                 <li className="border-gray-400 border p-6" key={filtered.id}>
-                  {" "}
-                  {filtered.rocket_name}{" "}
+                  {filtered.rocket_name}
                 </li>
-              ))}{" "}
-        </ul>{" "}
-      </section>{" "}
+              ))}
+        </ul>
+      </section>
       <section>
         <h2 className="text-2xl w-full flex justify-center items-center p-3 font-bold">
-          My Missions{" "}
-        </h2>{" "}
+          My Missions
+        </h2>
         <ul className=" flex flex-col px-6 py-3">
-          {" "}
-          {missions &&
-            missions
+          {missions
+            && missions
               .filter((mission) => mission.isReserved === true)
               .map((reserve) => (
                 <li className="border-gray-400 border  p-6" key={reserve.id}>
-                  {" "}
-                  {reserve.mission_name}{" "}
+                  {reserve.mission_name}
                 </li>
-              ))}{" "}
-        </ul>{" "}
-      </section>{" "}
+              ))}
+        </ul>
+      </section>
       <section>
         <h2 className="text-2xl w-full flex justify-center items-center p-3 font-bold">
-          My Dragons{" "}
-        </h2>{" "}
+          My Dragons
+        </h2>
         <ul className=" flex flex-col px-6 py-3">
-          {" "}
-          {dragons &&
-            dragons
+          {dragons
+            && dragons
               .filter((dragon) => dragon.reserved === true)
               .map((reserve) => (
                 <li className="border-gray-400 border  p-6" key={reserve.id}>
-                  {" "}
-                  {reserve.name}{" "}
+                  {reserve.name}
                 </li>
-              ))}{" "}
-        </ul>{" "}
-      </section>{" "}
+              ))}
+        </ul>
+      </section>
     </div>
   );
 };
